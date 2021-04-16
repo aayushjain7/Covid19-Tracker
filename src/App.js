7317cba06e3743a80c8b04863fe80e1a50a0dcae
Aayush Jain <jaayush014@gmail.com>
2021-04-16 22:31:23 +0530

    Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { FormControl, Select, MenuItem, Card, CardContent } from '@material-ui/core';
+import InfoBox from './components/InfoBox';
+import Map from './components/Map';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className='app'>
-      <h1>COVID 19 TRACKER!</h1>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER!</h1>
+          <FormControl className='app__dropdown'>
+            <Select variant='outlined' value={country} onChange={onCountryChange}>
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox title='Coronavirus Cases' cases={123} total={2000} />
+          <InfoBox title='Recovered' cases={1234} total={3000} />
+          <InfoBox title='Deaths' cases={1253} total={5000} />
+        </div>
+
+        <Map />
+      </div>
+
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live cases by County</h3>
+          <h3>Worldwide cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
